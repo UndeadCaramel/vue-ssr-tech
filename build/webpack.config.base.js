aee64ad771600efaf320d0c31945781f7970daf9
UndeadCaramel <jwpbt.4481@gmail.com>
2019-03-28 22:57:34 +0800

    项目目录升级

---
+++ build/webpack.config.base.js
@@ -0,0 +1,46 @@
+const path=require('path')
+
+const isDev=process.env.NODE_ENV==="development"
+
+const config={
+    mode:process.env.NODE_ENV||"development",
+    target:'web',
+    entry:path.join(__dirname,'../src/index.js'),
+    output:{
+        filename:'bundle.[hash:8].js',
+        path:path.join(__dirname,'dist'),
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                use:'vue-loader'
+            },
+            {
+                test:/\.jsx$/,
+                use:'babel-loader'
+            },
+            {
+                test:/\.js$/,
+                use:'babel-loader',
+                exclude:/node_modules/,
+                include:__dirname+'src'
+            },
+            {
+                test:/\.(gif|jpg|jpeg|png|svg)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            limit:1024,
+                            name:'resource/[path][name][hash:8].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+}
+
+module.exports=config;
---
+++ build/webpack.config.client.js
@@ -0,0 +1,104 @@
+const path=require('path')
+const VueLoaderPlugin=require('vue-loader/lib/plugin.js')
+const HTMLPlugin=require('html-webpack-plugin')
+const webpack=require('webpack')
+const merge=require('webpack-merge')
+const ExtractPlugin=require('extract-text-webpack-plugin')
+
+const isDev=process.env.NODE_ENV==="development"
+
+const baseConfig=require('./webpack.config.base.js')
+
+const devServer={
+    port:"8080",
+    host:"0.0.0.0",
+    overlay:{
+        error:true,
+    },
+    hot:true
+}
+
+let config
+
+if(isDev){
+    config=merge(baseConfig,{
+        devtool:"#cheap-module-eval-source-map",
+        devServer,
+        module:{
+            rules:[
+                {
+                    test:/\.styl(us)?$/,
+                    use:[
+                        'style-loader',
+                        'css-loader',
+                        {
+                            loader:'postcss-loader',
+                            options:{
+                                sourceMap:true,
+                            }
+                        },
+                        'stylus-loader'
+                    ]
+                },
+            ]
+        },
+        plugins:[
+            new webpack.HotModuleReplacementPlugin(),
+            new VueLoaderPlugin(),
+            new webpack.DefinePlugin({
+                'process.env':{
+                    NODE_ENV: isDev ? '"develpoment"' : '"production"'
+                }
+            }),
+            new HTMLPlugin()
+        ]
+    }) 
+}else{
+    config=merge(baseConfig,{
+        entry:{
+            app:path.join(__dirname,"../src/index.js"),
+            // vendor:['vue']
+        },
+        output:{
+            filename:'[name].[chunkhash:8].js' 
+        },
+        module:{
+            rules:[
+                {
+                    test:/\.styl(us)?$/,
+                    use:ExtractPlugin.extract({
+                        fallback:'style-loader',
+                        use:[
+                            'css-loader',
+                            {
+                                loader:'postcss-loader',
+                                options:{
+                                    sourceMap:true
+                                }
+                            },
+                            'stylus-loader'
+                        ]
+                    })
+                },
+            ]
+        },
+        optimization:{
+            splitChunks:{
+                chunks:'all'
+            },
+            runtimeChunk:true
+        },
+        plugins:[
+            new ExtractPlugin('styles.[chunkHash:8].css'),
+            new VueLoaderPlugin(),
+            new webpack.DefinePlugin({
+                'process.env':{
+                    NODE_ENV: isDev ? '"develpoment"' : '"production"'
+                }
+            }),
+            new HTMLPlugin()
+        ]
+    })
+}
+
+module.exports=config;
